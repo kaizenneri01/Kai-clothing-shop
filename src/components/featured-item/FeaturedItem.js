d5cfc1514af4e85875d7b5e041fe9d9c4189f5d0
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-02 16:38:02 +0800

    adding featured item component and adding styles

--- src/components/featured-item/FeaturedItem.js
+++ src/components/featured-item/FeaturedItem.js
@@ -3,18 +3,19 @@ import "./featured-item-styles.css";
 
 const FeaturedItem = () => {
   return (
-    <div className="featured__container">
-      <div>
-        <img src="#" alt="tshirt" />
-        <h1>PLAIN T-SHIRT</h1>
-      </div>
-      <div>
-        <img src="#" alt="colored shirt" />
-        <h1>COLORED SHIRT</h1>
-      </div>
-      <div>
-        <img src="#" alt="tshirt" />
-        <h1>OVERSIZED SHIRT</h1>
+    <div className="featured__item">
+      <div className="featured__font">FEATURED</div>
+      <div className="featured__container">
+        <div className="featured__picture">
+          <img
+            src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"
+            alt="tshirt"
+          />
+          <div className="featured__text">
+            <p>PLAIN T-SHIRT</p>
+            <p>SEE ALL</p>
+          </div>
+        </div>
       </div>
     </div>
   );
