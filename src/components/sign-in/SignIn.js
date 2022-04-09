d4fd64e05fba3f9a3b712476e032f41196924aba
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-09 20:33:17 +0800

    adding sign-in and sign-up components to my homepage

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,11 +1,32 @@
 import React from "react";
+import "./sign-in-styles.css";
 
 const SignIn = ({ open, onClose }) => {
   if (!open) return null;
   return (
-    <div>
-      <button onClick={onClose}>close</button>
-      SignIn
+    <div className="signIn__overlay">
+      <div className="signIn__modal">
+        <img src="#" alt="logo" />
+        <form className=" signIn__form">
+          <label>
+            {" "}
+            Username:
+            <input />{" "}
+          </label>
+          <label>
+            {" "}
+            Password:
+            <input />{" "}
+          </label>
+          <label>
+            {" "}
+            Remeber me:
+            <input />{" "}
+          </label>
+        </form>
+
+        <button onClick={onClose}>close</button>
+      </div>
     </div>
   );
 };
