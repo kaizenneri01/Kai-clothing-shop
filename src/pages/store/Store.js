63089a02a42e395a37b1c4d74151d3f9674cce97
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-09 17:52:44 +0800

    adding sign-in and sign-up components

--- src/pages/store/Store.js
+++ src/pages/store/Store.js
@@ -13,27 +13,27 @@ const Store = () => {
     {
       image:
         "https://images.unsplash.com/photo-1568306281824-7afe898030d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      description: "JACKET",
+      description: "SUIT",
     },
     {
       image:
         "https://images.unsplash.com/photo-1527718641255-324f8e2d0421?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-      description: "JACKET",
+      description: "PRINT SHIRT",
     },
     {
       image:
         "https://images.unsplash.com/photo-1525828024186-5294af6c926d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      description: "JACKET",
+      description: "PLAIN SHIRT",
     },
     {
       image:
         "https://images.unsplash.com/photo-1636458938604-38cd6718ee43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-      description: "JACKET",
+      description: "SWEATER",
     },
     {
       image:
         "https://images.unsplash.com/photo-1525457136159-8878648a7ad0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      description: "JACKET",
+      description: "POLO",
     },
   ]);
 
@@ -42,7 +42,6 @@ const Store = () => {
       <NavigationBar />
       <div className="store__line">
         <h1 className="store__font">STORE</h1>
-        <input className="store__searchbar" type="text" />
         <h1 className="store__category">CATEGORY</h1>
       </div>
 
@@ -50,7 +49,7 @@ const Store = () => {
         {data.map((item) => (
           <div className="store__box">
             <img className="store__image" src={item.image} alt="cloth" />
-            <h1 className="store__text">JACKET</h1>
+            <h1 className="store__text">{item.description}</h1>
           </div>
         ))}
       </div>
