68e3c72492d4054a7b9dba0b6bfa5ad6fce98730
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-10 17:25:09 +0800

    adding product component to my jacket section

--- src/components/jacket/Jacket.js
+++ src/components/jacket/Jacket.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationBar from "../navigationbar/NavigationBar";
 import Product from "../product/Product";
 import "./jacket-styles.css";
 
 const Jacket = () => {
+  const [jacketData] = useState([
+    {
+      image:
+        "https://www.pngall.com/wp-content/uploads/11/Black-Pullover-PNG.png",
+      title: "BLACK HOODIE JACKET ",
+      price: "25 $",
+    },
+    {
+      image: "https://pngimg.com/uploads/hoodie/hoodie_PNG23.png",
+      title: "PINK HOODIE JACKET",
+      price: "25$",
+    },
+    {
+      image:
+        "https://ramptshirts.com/assets/images/samples/standard-hoodies/red.png",
+      title: "RED HOODIE JACKET",
+      price: "25$",
+    },
+    {
+      image:
+        "https://ramptshirts.com/assets/images/samples/standard-hoodies/orange.png",
+      title: "ORANGE HOODIE JACKET",
+      price: "25$",
+    },
+  ]);
   return (
     <div>
       <NavigationBar />
-      <Product />
+      <div className="jacket__font">
+        <h1>JACKET</h1>
+      </div>
+      <div className="jacket__container">
+        {jacketData.map((data, index) => (
+          <Product key={index} info={data} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,26 +1,21 @@
 import React from "react";
 import "./product-styles.css";
 
-const Product = () => {
+const Product = ({ info }) => {
   return (
-    <div>
-      <div className="product__container">
-        <div className="product__product">
-          <img
-            className="product__image"
-            src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"
-            alt="product"
-          />
-          <div className="product__size">Size</div>
-          <ul className="product__details">
-            <li>S</li>
-            <li>M</li>
-            <li>L</li>
-            <li>XL</li>
-          </ul>
-          <div className="product__price">10 $</div>
-          <button className="product__button">BUY NOW</button>
-        </div>
+    <div className="product__container">
+      <div className="product__product">
+        <img className="product__image" src={info.image} alt="product" />
+        <div className="product__title">{info.title}</div>
+        <div className="product__size">Size</div>
+        <ul className="product__details">
+          <li>S</li>
+          <li>M</li>
+          <li>L</li>
+          <li>XL</li>
+        </ul>
+        <div className="product__price">{info.price}</div>
+        <button className="product__button">BUY</button>
       </div>
     </div>
   );
