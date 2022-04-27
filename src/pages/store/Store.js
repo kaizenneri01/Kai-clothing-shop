5fe54f019a0b03d0ef7cf93c6129202a30296f96
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-27 14:25:59 +0800

    adding react route in store page

--- src/pages/store/Store.js
+++ src/pages/store/Store.js
@@ -1,44 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./store-styles.css";
 
 import NavigationBar from "../../components/navigationbar/NavigationBar";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Store = () => {
-  const [data, setData] = useState([
+  let navigate = useNavigate();
+
+  const [data] = useState([
     {
       image:
         "https://images.unsplash.com/photo-1523398002811-999ca8dec234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=705&q=80",
-      description: "JACKET",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          JACKET
+        </Link>
+      ),
     },
     {
       image:
         "https://images.unsplash.com/photo-1568306281824-7afe898030d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      description: "SUIT",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          SUITS
+        </Link>
+      ),
     },
     {
       image:
         "https://images.unsplash.com/photo-1527718641255-324f8e2d0421?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-      description: "PRINT SHIRT",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          T-SHIRT
+        </Link>
+      ),
     },
     {
       image:
         "https://images.unsplash.com/photo-1525828024186-5294af6c926d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      description: "PLAIN SHIRT",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          PLAIN SHIRT
+        </Link>
+      ),
     },
     {
       image:
         "https://images.unsplash.com/photo-1636458938604-38cd6718ee43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-      description: "SWEATER",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          SWEATER
+        </Link>
+      ),
     },
     {
       image:
         "https://images.unsplash.com/photo-1525457136159-8878648a7ad0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      description: "POLO",
+      link: (
+        <Link className="store__link" to="/Store/Jacket">
+          POLO
+        </Link>
+      ),
     },
   ]);
-
-  let navigate = useNavigate();
 
   return (
     <div>
@@ -50,9 +74,9 @@ const Store = () => {
 
       <div className="store__container">
         {data.map((item, index) => (
-          <div key={index} className="store__box">
+          <div key={index} className="store__box" onClick={() => item.link}>
             <img className="store__image" src={item.image} alt="cloth" />
-            <h1 className="store__text">{item.description}</h1>
+            <h1 className="store__text">{item.link}</h1>
           </div>
         ))}
       </div>
