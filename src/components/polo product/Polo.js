fb409420468875fd3ca1445cbbb3e8a511d77f2f
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-04-27 18:30:34 +0800

    adding polo components and adding styles to my store page

--- src/components/polo product/Polo.js
+++ src/components/polo product/Polo.js
@@ -6,48 +6,51 @@ import "./polo-styles.css";
 const Polo = () => {
   const [poloData] = useState([
     {
-      image: "https://pngimg.com/uploads/sweater/sweater_PNG50.png",
-      title: "GRAY SWEATER",
+      image:
+        "https://o.remove.bg/downloads/c126f482-1487-47a3-8e8d-43dc22a2a96a/image-removebg-preview.png",
+      title: "RED POLO",
       price: "25 $",
     },
     {
-      image: "https://pngimg.com/uploads/sweater/sweater_PNG9.png",
-      title: "GRAY JUMPER SWEATER",
+      image:
+        "https://purepng.com/public/uploads/large/plain-black-polo-shirt-pab.png",
+      title: "BLACK POLO",
       price: "20$",
     },
     {
-      image: "https://www.downloadclipart.net/large/sweater-png-hd.png",
-      title: "YELLOWISH SWEATER",
+      image:
+        "https://www.pngall.com/wp-content/uploads/2016/04/Polo-Shirt-Free-Download-PNG.png",
+      title: "GRAY POLO",
       price: "21$",
     },
     {
       image:
-        "https://o.remove.bg/downloads/94d07821-2ad7-4434-8480-90bade4145fa/image-removebg-preview.png",
-      title: "PINK SWEATER",
+        "https://www.prada.com/content/dam/pradanux_products/U/UJN/UJN714/1YDPF0002/UJN714_1YDP_F0002_S_211_SLF.png",
+      title: "BLACK POLO",
       price: "15$",
     },
     {
       image:
-        "https://www.pngkit.com/png/full/117-1178063_sweater-png-pic-red-sweater-from-the-story.png",
-      title: "RED SWEATER",
+        "https://o.remove.bg/downloads/c81c2841-79c5-4809-8db4-c42d6a9405b2/image-removebg-preview.png",
+      title: "GREEN POLO",
       price: "15$",
     },
     {
-      image: "https://www.picng.com/upload/sweater/png_sweater_27852.png",
-      title: "BLUE JUMPER SWEATER",
+      image:
+        "https://www.pngkit.com/png/full/836-8369889_some-details-about-performance-golf-polo-shirts-polo.png",
+      title: "ORANGE POLO",
       price: "20$",
     },
     {
-      image:
-        "https://www.pngplay.com/wp-content/uploads/2/Sweater-PNG-HD-Quality.png",
-      title: "BROWN SWEATER",
+      image: "https://pngimg.com/uploads/polo_shirt/polo_shirt_PNG8140.png",
+      title: "SKY BLUE POLO",
       price: "25$",
     },
     {
       image:
-        "https://www.pngkey.com/png/full/53-536559_stranger-things-merry-christmas-ugly-sweater-overwatch-ugly.png",
-      title: "MERRY CHRISTMAS SWEATER",
-      price: "500$",
+        "https://www.sportyfied.com/thumbs/regular/028250_55_manhattanpolo_f_700x700.png",
+      title: "BLUE POLO",
+      price: "30$",
     },
   ]);
 
